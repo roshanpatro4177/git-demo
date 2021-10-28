486aa2fdf237750618fb7b95893b6ca97bb62af5
roshanpatro4177 <roshanrpatro@gmail.com>
2021-10-28 22:55:37 +0530

    added new file and modified old file

--- src/main/java/com/roshan/spring/jenkins/GitDemoApplication.java
+++ src/main/java/com/roshan/spring/jenkins/GitDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		SpringApplication.run(GitDemoApplication.class, args);
 	}
 
---
+++ src/main/java/com/roshan/spring/jenkins/contorller/HelloContorller.java
@@ -0,0 +1,14 @@
+package com.roshan.spring.jenkins.contorller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContorller {
+	
+	@GetMapping("/hello")
+	public String getName() {
+		return "Hello World";
+	}
+
+}
